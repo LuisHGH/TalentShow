9a727a531cd5991d0a087ff1ed6885ab9390017f
Diegovsky <46163903+Diegovsky@users.noreply.github.com>
2019-07-31 15:26:07 -0300

    Update TalentShow.java

--- TalentShow.java
+++ TalentShow.java
@@ -1,33 +1,53 @@
-import java.util.*;
-import static java.util.Collectors.*;
- //Compiler version JDK 1.8
+package TalentShow;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import TalentShow.Team;
+
  class TalentShow{ 
 	 public static void main(String args[]){ 	
 		ArrayList<Team> lista = new ArrayList();		
 		Scanner teclado = new Scanner (System.in);
-		System.out.println("Bem vindo ao Talent Show Points Administrator:");
-		for(int cont = 0;op != 2;cont++) {
-			System.out.println("\nDigite qual das opções você deseja:");
-			System.out.println(" 1 - Adicionar  uma equipe\n 2 - Mostrar a classificação final\n ");
-			int op = teclado.nextInt();
-			if(op == 1){
-				Team team = new Team();
-				lista.add(team);
-			}else if(op == 2) {
-				List l = lista
-				.stream()
-				.sorted( (x, y) -> Comparator.compare(x.soma, y.soma) )
-				.collect(toList());
-				for(int i=1; i <= l.length(); i++) {
-					System.out.println(String.valueOf(i) + "º lugar: " + l.get(i-1).toString() + "\n");
-					for(int j=0; j < l.get(i).length(); j++) System.out.println(l.get(i).name);
-					System.out.println();
-				}
-			}else {
-				System.out.println("[ERRO] Digite uma opção válida:");
-				continue;
-			}
-				
+		System.out.println("Bem vindo ao Talent Show Points Administrator\n");
+		System.out.println("Digite qual das opções você deseja:");
+		System.out.println(" 1 - Adicionar  uma equipe\n 2 - Mostrar a classificação final\n ");
+		while(int op = teclado.nextInt()){			
+			if(op == 1)
+			lista.add(createTeam());
+			else if(op == 2) break;
+			else System.out.println("Opção inválida");
 		}
+		if(lista == null){
+			System.out.println("Lista nula");
+			return;
+		}
+		List l = lista
+		.stream()
+		.sorted( (x, y) -> Comparator.compare(x.soma, y.soma) )
+		.collect(Collectors.toList());
+		for(int i=1; i <= l.length(); i++) {
+			System.out.println(String.valueOf(i) + "º lugar: " + l.get(i-1).toString() + "\n");
+			for(int j=0; j < l.get(i-1).part.length(); j++) System.out.println(l.get(i).name);
+			System.out.println();
+		}
+						
+	 }
+	 
+	 public Team createTeam(){
+		Scanner teclado = new Scanner(System.in);
+		String nome;
+		ArrayList<String> part = new ArrayList<String>();
+		ArrayList<Integer> react;
+		System.out.println("Digite o nome do time: "):
+		nome = teclado.nextLine();
+		System.out.println("Digite o nome dos participantes. Caso acabe, insira uma linha em branco.");
+		while(teclado.hasNext()) part.add(teclado.nextLine());
+		System.out.println("Digite as medidas das reações. Caso acabe, insira uma linha em branco.");
+		while(teclado.hasNext()){
+			react.add(teclado.nextInt());
+			teclado.nextLine();
+		}
+		return new Team(nome, part, react.stream().mapToInt(i->i).toArray());
+		
 	 }
 }
