7a7b8f7c3b12a7b2c0b0261861c0e9a825d0d6bb
LuisHGH <luishenriquegh2701@gmail.com>
2020-07-01 10:35:52 -0300

    Refactored using impl notation

---
+++ out/production/TalentShow/README.md
@@ -0,0 +1,3 @@
+# TalentShow
+
+Nanis is a brazilian programmer who likes to freelance for his teachers. Using all of his free time and good will, Nanis charges nothing for his services and gets nothing in return. This project is what we made in a couple of hours for our english teacher. It registers teams and participants, then prints out which team has the best score.
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
         println!("\nType which of the following options you desire:");
         let buffer = read_line(" 1 - Add a Team\n 2 - Show final results\n");   
         match buffer.as_str().trim(){
-            "1" => add_team(&mut vec),
+            "1" => Team::add_team(&mut vec),
             "2" => {
-                print_results(&mut vec);
+                Team::print_results(&mut vec);
                 break;
             },
             &_ => println!("[ERROR] Type a valid option:")
@@ -25,72 +25,76 @@ fn read_line(string: &str,) -> String{
         .expect("Failed to read line");
     buffer
 }
-fn add_team(vec: &mut Vec<Team>){
-    let team_name = read_line("Type the team's name:");
-    let mut vec_names: Vec<String> = Vec::new();
-    println!("Type participants' names and a blank line when done");
-    loop{
-        let mut buffer = String::new();
-        io::stdin()
-            .read_line(&mut buffer)
-            .expect("Failed to read participant's name!");
-        if buffer.trim().is_empty() {
-            break;
-        } else{
-            vec_names.push(buffer);
-        }
-    }
-    let mut reactions = [0i32; 4];
-    for i in 0..4{
-        let mut buffer = String::new();
-        println!("Type a value between 0 to 5 for {}:", REACTION_TYPES[i]);
-        reactions[i] = loop{
-                io::stdin()    
-                .read_line(&mut buffer)
-                .expect("Failed to read value");
-            match buffer.trim().parse::<i32>() {
-                Ok(num) => match num {
-                    0..=5 => break num,
-                    _ => {
-                        println!("The score must be between 0 and 5, please try again:");
-                        continue
-                    },
-                }
-                Err(_) => {
-                    println!("The score must be an unsigned integer, please try again:");
-                    continue
-                },
-            };
-        };
-    }
-    println!("Team {} created", team_name);
-    let new_team = Team {
-        name: team_name,
-        participants: vec_names,
-        sum: reactions.iter().sum(),
-        reactions: reactions,
-    };
-    vec.push(new_team);
-}
-fn print_results(vec: &mut Vec<Team>){
-    vec.sort_by( |x, y|  y.sum.partial_cmp(&x.sum).unwrap() );
-    println!("\n------------------");
-    for (i, team ) in vec.iter().enumerate(){
-        println!("{}º place: {}\n\nParticipants:", i + 1, team.name);
-        for name in team.participants.iter(){
-            println!("\t-{}", name);
-        }
-        println!("\nTotal: {}\n", team.sum);
-        for (i, score) in team.reactions.iter().enumerate(){
-            println!("{} value: {}", REACTION_TYPES[i], score);
-        }
-        println!("\n------------------\n");
-    }
-}
-
 struct Team{
    pub name: String,
    pub reactions: [i32; 4],
    pub sum: i32,
    pub participants: Vec<String>,
+}
+impl Team{
+    fn add_team(vec: &mut Vec<Team>){
+        let name = read_line("Type the team's name:");
+        let mut participants: Vec<String> = Vec::new();
+        println!("Type participants' names and a blank line when done");
+        loop{
+            let mut buffer = String::new();
+            io::stdin()
+                .read_line(&mut buffer)
+                .expect("Failed to read participant's name!");
+            if buffer.trim().is_empty() {
+                break;
+            } else{
+                participants.push(buffer);
+            }
+        }
+        let mut reactions = [0i32; 4];
+        for i in 0..4{
+            let mut buffer = String::new();
+            println!("Type a value between 0 to 5 for {}:", REACTION_TYPES[i]);
+            reactions[i] = loop{
+                    io::stdin()    
+                    .read_line(&mut buffer)
+                    .expect("Failed to read value");
+                match buffer.trim().parse::<i32>() {
+                    Ok(num) => match num {
+                        0..=5 => break num,
+                        _ => {
+                            println!("The score must be between 0 and 5, please try again:");
+                            continue
+                        },
+                    }
+                    Err(_) => {
+                        println!("The score must be an unsigned integer, please try again:");
+                        continue
+                    },
+                };
+            };
+        }
+        println!("\nTeam {} created", name);
+        let new_team = Team {
+            name,
+            participants,
+            sum: reactions.iter().sum(),
+            reactions,
+        };
+        vec.push(new_team);
+    }
+    fn print_placing(&self, i: usize){
+        println!("{}º place: {}\n\nParticipants:", i + 1, self.name);
+            for name in self.participants.iter(){
+                println!("\t-{}", name);
+            }
+            println!("\nTotal: {}\n", self.sum);
+            for (i, score) in self.reactions.iter().enumerate(){
+                println!("{} value: {}", REACTION_TYPES[i], score);
+            }
+            println!("\n------------------\n");
+    }
+    fn print_results(vec: &mut Vec<Team>){
+        vec.sort_by( |x, y|  y.sum.partial_cmp(&x.sum).unwrap() );
+        println!("\n------------------");
+        for (i, team ) in vec.iter().enumerate(){
+            team.print_placing(i);
+        }
+    }
 }
